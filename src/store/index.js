4dc4b147e40687569d766311271fe528d5525b50
Daniel <daniel.montellanos@entelgy.com>
2022-11-05 12:29:34 -0500

    scaffolding proyect vite vue2 bibliotecaonline

---
+++ src/store/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue';
+import Vuex, { createLogger } from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  modules: {},
+  plugins: [createLogger()],
+  state: {
+    count: 0
+  },
+  getters: {
+    count(state) {
+      return state.count;
+    },
+    doubleCount(state) {
+      return state.count * 2;
+    }
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    }
+  },
+  actions: {
+    increment({ commit }) {
+      commit('increment');
+    }
+  }
+});
